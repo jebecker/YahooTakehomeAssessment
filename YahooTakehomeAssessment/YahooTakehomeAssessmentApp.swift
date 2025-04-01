1198df5754e4751b3fe3f979d8cfedd24d900874
Jayme Becker <>
2025-04-01 09:18:20 -0500

    Initial commit

---
+++ YahooTakehomeAssessment/YahooTakehomeAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahooTakehomeAssessmentApp.swift
+//  YahooTakehomeAssessment
+//
+//  Created by Jayme Becker on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct YahooTakehomeAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
